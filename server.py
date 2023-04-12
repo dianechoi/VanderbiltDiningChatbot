1ea37c104abaedaf6d5f385cd502c029c5639340
dianechoi <diane.choi@vanderbilt.edu>
2023-04-11 21:44:16 -0500

    new temp

--- server.py
+++ server.py
@@ -1,48 +1,83 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, redirect
 from kubernetes import client, config
+import time
 
 app = Flask(__name__)
 i = 0
+
+
 @app.route('/')
 def index():
-  return render_template('index.html', name="")
+    return render_template('index.html', name="")
+
 
 @app.route('/my-link/')
 def my_link():
-  global i
-  i += 1
-  print ('I got clicked!')
+    global i
+    i += 1
+    print('I got clicked!')
 
-  # Load the Kubernetes configuration from the service account
-  config.load_incluster_config()
+    config.load_incluster_config()
 
-  # Create a Kubernetes API client
-  api = client.CoreV1Api()
+    deployment = client.V1Deployment()
+    deployment.api_version = "apps/v1"
+    deployment.kind = "Deployment"
+    deployment.metadata = client.V1ObjectMeta(name="nginx-deployment-" + str(i))
+    deployment.spec = client.V1DeploymentSpec(
+        replicas=1,
+        selector=client.V1LabelSelector(
+            match_labels={"app": "nginx-" + str(i)}
+        ),
+        template=client.V1PodTemplateSpec(
+            metadata=client.V1ObjectMeta(
+                labels={"app": "nginx-" + str(i)}
+            ),
+            spec=client.V1PodSpec(
+                containers=[
+                    client.V1Container(
+                        name="nginx-" + str(i),
+                        image="nginx:latest",
+                        ports=[client.V1ContainerPort(container_port=80)]
+                    )
+                ]
+            )
+        )
+    )
 
-  # Define the container spec
-  container_spec = client.V1Container(
-      name='my-container',
-      image='nginx:latest'
-  )
+    api_instance = client.AppsV1Api()
+    api_instance.create_namespaced_deployment(
+        body=deployment,
+        namespace="default"
+    )
 
-  # Define the pod spec
-  pod_spec = client.V1PodSpec(
-      containers=[container_spec]
-  )
+    service = client.V1Service()
+    service.api_version = "v1"
+    service.kind = "Service"
+    serviceName = "nginx-service-" + str(i)
+    service.metadata = client.V1ObjectMeta(name=serviceName)
+    service.spec = client.V1ServiceSpec(
+        selector={"app": "nginx-" + str(i)},
+        ports=[client.V1ServicePort(port=80, target_port=80, )],
+        type="NodePort"
+    )
 
-  # Define the pod object
-  pod = client.V1Pod(
-      metadata=client.V1ObjectMeta(
-          name='my-pod-'+str(i)
-      ),
-      spec=pod_spec
-  )
+    api_instance = client.CoreV1Api()
+    api_instance.create_namespaced_service(
+        body=service,
+        namespace="default"
+    )
+    api = client.CoreV1Api()
+    time.sleep(0.1)
+    service = api.read_namespaced_service(name=serviceName, namespace='default')
 
-  # Create the pod
-  api.create_namespaced_pod(namespace='default', body=pod)
-  return 'Click.'
+    # Get the nodePort of the Service
+    for port in service.spec.ports:
+        return redirect(f'129.114.26.125:{port.node_port}', code=302)
 
 
-    
+    return "Something went wrong. Please try again!"
+
+
 if __name__ == '__main__':
-  app.run(debug=True, host='0.0.0.0')
+    app.static_folder = 'static'
+    app.run(debug=True, host='0.0.0.0')
